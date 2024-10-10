d97fa589f576ae1f7b25a55764af9960b8328ca6
rhakash008 <rhakash008@gmail.com>
2024-10-10 15:40:22 +0600

    Create 705A.cpp

---
+++ 705A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i;
+    
+    cin>>n;
+    for(i=1;i<=n;i++){
+        if(i==1){
+            cout<<"I hate";
+        } 
+        else if(i%2!=0){
+            cout<<" that I hate";
+        }
+        else{
+            cout<<" that I love";
+        }
+        }
+    
+    cout<<" it";
+    return 0;
+}
